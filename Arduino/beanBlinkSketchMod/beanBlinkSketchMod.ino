5ff219c8f0e9352a1c2198b1fb415481ff5e2a9e
Nadine <nfarah86@gmail.com>
2016-03-22 16:24:37 -0700

    modified arduino code from matt's review on bean doc

--- Arduino/beanBlinkSketchMod/beanBlinkSketchMod.ino
+++ Arduino/beanBlinkSketchMod/beanBlinkSketchMod.ino
@@ -4,27 +4,18 @@
 // 1 (LED is ON) or 0 (LED is OFF). 
 
 void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  // We don't need to do anything here
 }
 
 void loop() {
-    while (Serial.available() > 0) {
-       char data = Serial.read();
-       if (data == 1) {
-         Bean.setLed(0,255,0);
-         delay(1000);
-         Bean.setLed(0,0,255);
-         delay(1000);
-         Bean.setLed(255,0,0);
-         delay(1000);
-         Bean.setLed(0,0,0);
-         delay(1000);
-       } else {
-         Bean.setLed(0,0,0);
-     }
+  while (Serial.available() > 0) {
+    char data = Serial.read();
+    if (data == 1) {
+      Bean.setLed(0, 255, 255);
+    } else {
+      Bean.setLed(0,0,0);
+    }
   }
-
-        delay(1000);
+  Bean.sleep(250);
 }
 
